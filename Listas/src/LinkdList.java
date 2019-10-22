6b162a667ceadcf71e193e43953c3a7e4797b279
mateusctm <mateusctm@gmail.com>
2019-10-22 14:43:00 -0300

    Add files via upload

---
+++ Listas/src/LinkdList.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mateu
+ */
+public class LinkdList {
+    private ListNode first;
+    private int size;
+    
+    LinkdList(){
+        first = null;
+        size = 0;
+    }
+    
+}
---
+++ Listas/src/ListNode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mateu
+ */
+public class ListNode {
+    private int val;
+    private ListNode next;
+    
+    public ListNode(int v, ListNode n){
+            val = v;
+            next = n;
+    }
+    
+    
+}
